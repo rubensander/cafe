76bc56ca42533ce7194395abd0f7ba2a6582b989
Ruben Sander <rubensander@gmx.de>
2020-03-22 16:52:56 +0100

    Init git repository with basic structure

---
+++ main.js
@@ -0,0 +1,8 @@
+function showDE() {
+    for (let seat of document.getElementsByClassName("table")) {
+        seat.children[0].src = "./img/DE_t.svg"
+    }
+}
+
+var img = new Image();
+img.src = "./img/DE_t.svg";
