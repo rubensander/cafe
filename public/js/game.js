c1baee9b84848f0d3b665b14863dc2e29608bf95
rubensander <rubensander@gmx.de>
2022-12-28 09:27:48 +0100

    don't use calculated vh

--- public/js/game.js
+++ public/js/game.js
@@ -306,8 +306,9 @@ function addHandcard(card) {
   //img.style.left = document.getElementById("game").width + i++ / msgObj.cards.length * 100 + "%";
   hand.appendChild(img);
 }
-
+/*
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+*/
